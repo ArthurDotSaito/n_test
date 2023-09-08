edce3790bcb7a0505b0d02bc37cc4ef21f292642
Arthur Saito <arthur_saito1998@hotmail.com>
2023-09-07 23:00:47 -0300

    feat: generating and reading one matrix on problem06/solution01

---
+++ 06/README.md
@@ -0,0 +1,3 @@
+## Question
+
+Implemente uma função ou trecho de código que receba uma matriz quadrada (mesmo número de linhas e colunas) e inverta os elementos da diagonal principal para a diagonal secundária. No exemplo abaixo, a diagonal principal é composta pelos elementos a(1,1); a(2,2) e a(3,3) e a diagonal secundária é composta pelos elementos a(1,3); a(2,2) e a(3,1).
---
+++ 06/solution01/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Question6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "matrix.txt";
+            int n = 3;
+            CreateMatrix(filename, n);
+            Console.WriteLine($"Matriz {n}x{n} gerada e salva em '{filename}'.");
+
+            int[,] matrix = ReadMatrix(filename);
+            Console.WriteLine("Matrix: ");
+            ShowMatrix(matrix);
+        }
+        static void CreateMatrix(string filename, int n)
+        {
+            Random r = new Random();
+            FileStream f = new FileStream(filename, FileMode.Create);
+            StreamWriter sw = new StreamWriter(f);
+            sw.Write($"{n} {n}\r\n");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; ++j)
+                {
+                    int element = r.Next(11);
+                    sw.Write($"{element} ");
+                }
+                sw.Write("\r\n");
+            }
+            sw.Close();
+            f.Close();
+        }
+        static int[,] ReadMatrix(string filename)
+        {
+            FileStream f = new FileStream(filename, FileMode.Open);
+            StreamReader sr = new StreamReader(f);
+            string[] dimension = sr.ReadLine().Split(' ');
+            int size = int.Parse(dimension[0]);
+            int[,] ret = new int[size, size];
+
+            for (int i = 0; i < size; ++i)
+            {
+                string line = sr.ReadLine();
+                string[] elements = line.Split(' ');
+
+                for (int j = 0; j < size; ++j)
+                {
+                    ret[i, j] = int.Parse(elements[j]);
+                }
+            }
+            sr.Close();
+            return ret;
+        }
+
+        static void ShowMatrix(int[,] matrix)
+        {
+            int size = matrix.GetLength(0);
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
